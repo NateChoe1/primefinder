aaa484a954f69bfbc55a36667f9312cc5ad65861
thetinybros@gmail.com <thetinybros@gmail.com>
2020-12-02 13:46:27 -0600

    Added 2 new prime finding methods, refactored code to fit python standards, improved code readability with the sacrifice of rounding up to the nearest vicinity.

--- primefinder.py
+++ primefinder.py
@@ -1,7 +1,7 @@
 import timeit
 import math
 
-def getGCD(n1, n2):
+def get_gcd(n1, n2):
     while (n1 != 0 and n2 != 0):
         if (n1 >= n2):
             n1 %= n2
@@ -9,10 +9,10 @@ def getGCD(n1, n2):
             n2 %= n1
     return max(n1, n2)
 
-def isCoprime(n1, n2):
-    return (getGCD(n1, n2) == 1)
+def is_comprime(n1, n2):
+    return (get_gcd(n1, n2) == 1)
 
-def validatePrime(n):
+def validate_prime(n):
     if (n < 2):
         return False
     if (n == 2):
@@ -23,7 +23,7 @@ def validatePrime(n):
     return True
 
 print("How many primes should we find?")
-primesToFind = int(input())
+primes_to_find = int(input())
 
 print("What should the base be for the coprime method?")
 base = int(input())
@@ -33,27 +33,69 @@ print("Starting coprime method")
 start = timeit.default_timer()
 
 coprimes = []
-totalPrimes = 0
+total_primes = 0
 for x in range(1, base):
-    if (isCoprime(x, base)):
+    if (is_comprime(x, base)):
         coprimes.append(x)
-    if (validatePrime(x)):
-        totalPrimes += 1
+        if (validate_prime(x)):
+            total_primes += 1
 
-currentVicinity = base
-while (True):
-    shouldQuit = False
+current_vicinity = base
+while total_primes < primes_to_find:
     for x in coprimes:
-        if (validatePrime(currentVicinity+x)):
-            totalPrimes += 1
-            if (totalPrimes == primesToFind):
-                shouldQuit = True
-                break
-    if (shouldQuit):
-        break
-    currentVicinity += base
+        if (validate_prime(current_vicinity+x)):
+            total_primes += 1
+    current_vicinity += base
 
 stop = timeit.default_timer()
 
 print("Time: ", stop - start)
-print("Total primes: ", totalPrimes)
+print("Total primes: ", total_primes)
+
+print("\nStarting sieve of eratosthenes")
+start = timeit.default_timer()
+primes_found = []
+iterator = 1
+while len(primes_found) < primes_to_find:
+    iterator += 1
+    is_prime = True
+    for prime in primes_found:
+        if iterator % prime == 0:
+            is_prime = False
+            break
+    if not is_prime:
+        continue
+    primes_found.append(iterator)
+stop = timeit.default_timer()
+
+print("Time: ", stop - start)
+print("Total primes: ", len(primes_found))
+
+print("\nStarting combined method")
+start = timeit.default_timer()
+
+primes_found = []
+coprimes = []
+for x in range(1, base):
+    if (is_comprime(x, base)):
+        coprimes.append(x)
+        if (validate_prime(x)):
+            primes_found.append(x)
+
+current_vicinity = base
+while len(coprimes) < primes_to_find:
+    for x in coprimes:
+        check = current_vicinity + x
+        is_prime = True
+        for prime in primes_found:
+            if check % prime == 0:
+                is_prime = False
+                break
+        if is_prime:
+            prines_found.append(check)
+    current_vicinity += base
+
+stop = timeit.default_timer()
+
+print("Time: ", stop - start)
+print("Total primes: ", total_primes)
